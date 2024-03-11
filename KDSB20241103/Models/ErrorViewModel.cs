75b523a41fc89caa4705fec52c1bc886bbb2e10c
KevinSilva777 <sb24002@esfe.agape.edu.sv>
2024-03-11 07:54:35 -0600

    Agregar archivos de proyecto.

---
+++ KDSB20241103/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KDSB20241103.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
