02a8b287e9bbeeb0845dbca2aa97808943d1e87e
KevinSilva777 <sb24002@esfe.agape.edu.sv>
2024-03-11 22:38:58 -0600

    Editar Terminado

--- KDSB20241103/Models/Script.sql
+++ KDSB20241103/Models/Script.sql
@@ -10,7 +10,7 @@ GO
 CREATE TABLE Cliente (
     IdCliente INT PRIMARY KEY IDENTITY(1,1),
     NombreCliente VARCHAR(100) NOT NULL,
-    TotalVenta DECIMAL(10,2) NOT NULL
+    FechaRegistro date NOT NULL
 );
 GO
 
